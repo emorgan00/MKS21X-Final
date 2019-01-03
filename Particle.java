d0b16ed217ff13cce32df8b14f7627fd23c8833a
emorgan00 <emorgan00@stuy.edu>
2019-01-03 16:56:54 -0500

    added basic getters/setters to Particle. particles are mutable.

--- Particle.java
+++ Particle.java
@@ -11,4 +11,24 @@ public class Particle { // like a vector, but mutable and has both position and 
 		return "Particle(pos: "+pos+", dir: "+dir+")";
 	}
 
+	public Vector dir() {
+		return dir;
+	}
+
+	public Vector pos() {
+		return pos;
+	}
+
+	public void setPos(Vector pos) {
+		this.pos = pos;
+	}
+
+	public void translate(Vector motion) {
+		pos = pos.add(motion);
+	}
+
+	public void translateInverse(Vector motion) {
+		pos = pos.subtract(motion);
+	}
+
 }
