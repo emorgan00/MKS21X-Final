2d83413c115122d453064d35acb72254527a3466
emorgan00 <emorgan00@stuy.edu>
2019-01-03 17:23:53 -0500

    added constant TERMINAL_RATIO in the camera so we know the dimensions of a terminal character. this can then be edit if stuff seems stretched. for now, i have assumed a character is in a 1:2 ratio of width:height

--- Camera.java
+++ Camera.java
@@ -7,6 +7,8 @@ public class Camera extends Particle {
 	private double[][] zbuffer; // for z-buffering
 	private String[][] displaybuffer; // holds what will be printed
 
+	public static final double TERMINAL_RATIO = 2; // value of (height / width) for a character on the terminal
+
 	public Camera(int height, int width) {
 		super(Vector.ZERO, Vector.UNIT_X);
 		this.scale = 0.5; // arbitrarily 45 degrees in each direction
