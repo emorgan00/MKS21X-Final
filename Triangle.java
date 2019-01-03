ea7b4dc28f4c9a1d19c0ecea8441dc11666f10e1
emorgan00 <emorgan00@stuy.edu>
2019-01-03 12:10:56 -0500

    added Triangle translations. Added a new constructor to prevent re-computing normals. note: Triangles are static, so Triangle.translate() will return a new Triangle instead of modifying an existing one.

--- Triangle.java
+++ Triangle.java
@@ -12,6 +12,14 @@ public class Triangle {
 		this.normal = b.subtract(a).crossProduct(c.subtract(a));
 	}
 
+	public Triangle(Vector a, Vector b, Vector c, Vector normal, Vector center) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.center = center;
+		this.normal = normal;
+	}
+
 	public String toString() {
 		return "Triangle("+a+", "+b+", "+c+")";
 	}
@@ -22,6 +30,10 @@ public class Triangle {
 
 	public Vector center() {
 		return this.center;
+	}
+
+	public Triangle translate(Vector r) { // slightly faster than creating a new Triangle, since we just copy over the normal vector
+		return new Triangle(a.add(r), b.add(r), c.add(r), normal, center.add(r));
 	}
 
 }
