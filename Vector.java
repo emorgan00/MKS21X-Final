4c9aa5723885e29a830815cac93857f5bf16ed30
emorgan00 <emorgan00@stuy.edu>
2019-01-03 10:04:18 -0500

    added constructors to Vector.java

--- Vector.java
+++ Vector.java
@@ -2,4 +2,16 @@ public class Vector {
 
 	private double x, y, z;
 
+	public Vector(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public Vector(Vector v) {
+		this.x = v.x;
+		this.y = v.y;
+		this.z = v.z;
+	}
+
 }
