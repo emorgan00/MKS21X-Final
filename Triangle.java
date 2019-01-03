ec6e63e2146d557db842f7e59120f7b3afb1c296
emorgan00 <emorgan00@stuy.edu>
2019-01-03 12:03:53 -0500

    stored 1./3 for efficiency

--- Triangle.java
+++ Triangle.java
@@ -2,12 +2,13 @@ public class Triangle {
 	
 	private Vector a,b,c;
 	private Vector normal,center;
+	public static final double oneThird = 1./3;
 
 	public Triangle(Vector a, Vector b, Vector c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
-		this.center = a.add(b).add(c).scale(1./3);
+		this.center = a.add(b).add(c).scale(oneThird);
 		this.normal = b.subtract(a).crossProduct(c.subtract(a));
 	}
 
