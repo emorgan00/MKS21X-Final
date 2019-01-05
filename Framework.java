51d45fbe438b9d5ac0f31081878d8a98f2b5ceec
emorgan00 <emorgan00@stuy.edu>
2019-01-05 14:34:36 -0500

    added main method to Framework

--- Framework.java
+++ Framework.java
@@ -18,8 +18,19 @@ public class Framework {
 		objects = new ArrayList<>();
 
 		screen.startScreen();
-
+		while (tick()) {
+			camera.clearBuffer();
+			for (Shape obj : objects) {
+				camera.render(obj);
+			}
+			camera.display();
+			camera.doResizeIfNecessary();
+		}
 		screen.stopScreen();
+	}
+
+	public static boolean tick() { // return false to exit the program. otherwise return true
+		return false;
 	}
 
 }
