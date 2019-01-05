0dc104508335f30bd5a3a230f5eef0a45a974485
emorgan00 <emorgan00@stuy.edu>
2019-01-05 14:51:52 -0500

    added setup() and tick() to Framework

--- Framework.java
+++ Framework.java
@@ -8,24 +8,27 @@ import java.util.concurrent.*;
 
 public class Framework {
 
-	public static Screen screen;
-	public static Camera camera;
-	public static ArrayList<Shape> objects;
-	public static final int MILLIS_PER_FRAME = 100;
+	private static Screen screen;
+	private static Camera camera;
+	private static ArrayList<Shape> objects;
+
+	private static final int MILLIS_PER_FRAME = 100;
 
 	public static void main(String[] args) throws IOException {
 		screen = new DefaultTerminalFactory().createScreen();
 		camera = new Camera(screen);
-		objects = new ArrayList<>();
+		objects = new ArrayList<>(); // any object added here will be drawn
 
 		screen.startScreen();
 		long clock = 0;
 
+		setup();
+
 		while (true) {
 			long stime = System.currentTimeMillis();
 
-			// actual code goes here
-			if (clock > 1000) break;
+			// actual code goes here:
+			if (!tick(clock)) break;
 
 			camera.doResizeIfNecessary();
 			camera.clearBuffer();
@@ -38,6 +41,16 @@ public class Framework {
 			while (System.currentTimeMillis()-stime < MILLIS_PER_FRAME) {}
 		}
 		screen.stopScreen();
+	}
+
+	public static void setup() { // run at program initialization.
+
+	}
+
+	public static boolean tick(long clock) { // run at the start of every frame. return false to exit.
+		if (clock > 100) return false;
+
+		return true;
 	}
 
 }
