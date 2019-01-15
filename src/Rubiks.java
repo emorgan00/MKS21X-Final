e99fdb14d88eb564366739ff1b29dabc9ce9062b
emorgan00 <emorgan00@stuy.edu>
2019-01-15 18:52:32 -0500

    moved Rubiks methods into Rubiks class that extends Particle

--- src/Rubiks.java
+++ src/Rubiks.java
@@ -1,35 +1,23 @@
+import graphics.*;
 import java.util.ArrayList;
 import com.googlecode.lanterna.*;
-import com.googlecode.lanterna.input.*;
-import com.googlecode.lanterna.screen.*;
-import com.googlecode.lanterna.terminal.*;
-import java.io.IOException;
-import graphics.*;
 
-public class Rubiks {
+public class Rubiks extends Particle implements Renderable {
 
-	private static Screen screen;
-	private static Camera camera;
-	private static ArrayList<Shape> cubes;
+	private ArrayList<Shape> cubes;
 
-	public static void main(String[] args) throws IOException {
-		screen = new DefaultTerminalFactory().createScreen();
-		camera = new Camera(screen);
-		cubes = new ArrayList<>(); // any object added here will be drawn
-
-		screen.startScreen();
-		long stime = System.currentTimeMillis();
-		int dt;
+	public Rubiks(Vector pos, int cubeRadius) {
+		super(pos, Vector.UNIT_X, Vector.UNIT_Y);
 
 		// Setting up initial triangles
-		Vector a = new Vector(0.9, 0.9, 0.9);
-		Vector b = new Vector(0.9, 0.9, -0.9);
-		Vector c = new Vector(0.9, -0.9, 0.9);
-		Vector d = new Vector(0.9, -0.9, -0.9);
-		Vector e = new Vector(-0.9, 0.9, 0.9);
-		Vector f = new Vector(-0.9, 0.9, -0.9);
-		Vector g = new Vector(-0.9, -0.9, 0.9);
-		Vector h = new Vector(-0.9, -0.9, -0.9);
+		Vector a = new Vector(cubeRadius, cubeRadius, cubeRadius);
+		Vector b = new Vector(cubeRadius, cubeRadius, -cubeRadius);
+		Vector c = new Vector(cubeRadius, -cubeRadius, cubeRadius);
+		Vector d = new Vector(cubeRadius, -cubeRadius, -cubeRadius);
+		Vector e = new Vector(-cubeRadius, cubeRadius, cubeRadius);
+		Vector f = new Vector(-cubeRadius, cubeRadius, -cubeRadius);
+		Vector g = new Vector(-cubeRadius, -cubeRadius, cubeRadius);
+		Vector h = new Vector(-cubeRadius, -cubeRadius, -cubeRadius);
 
 		Triangle r0 = new Triangle(a, e, b, new TextColor.RGB(255, 0, 0));
 		Triangle r1 = new Triangle(e, f, b, new TextColor.RGB(255, 0, 0));
@@ -43,10 +31,6 @@ public class Rubiks {
 		Triangle w1 = new Triangle(d, f, h, new TextColor.RGB(255, 255, 255));
 		Triangle y0 = new Triangle(a, c, e, new TextColor.RGB(255, 255, 0));
 		Triangle y1 = new Triangle(c, g, e, new TextColor.RGB(255, 255, 0));
-
-		// Forward View:
-		camera.setPos(new Vector(-10, 6, 0));
-		camera.rotate(Vector.UNIT_Z, -Math.PI/6);
 
 		// setup the cube
 		for (int x = -2; x < 3; x += 2) {
@@ -62,108 +46,36 @@ public class Rubiks {
 					cube.translate(new Vector(x, y, z));
 					cube.setDir(cube.pos());
 					cube.setCenter(Vector.ZERO);
+					cube.translate(pos);
 					cubes.add(cube);
 				}
 			}
 		}
-
-		double anglebuffer = 0;
-		boolean scramble = false; // when true, we scramble the cube
-		int view = 0;
-		Vector rot = Vector.UNIT_Z; // which axis to rotate a face around
-		Vector face = Vector.ZERO; // which face to rotate. ZERO means all faces
-		KeyStroke key = null;
-
-		while (true) {
-			if (!scramble) key = screen.pollInput();
-			dt = (int)(System.currentTimeMillis()-stime);
-			stime = System.currentTimeMillis();
-
-			// clock is the index of the frame we are on.
-			// dt is the number of millis passed since the last tick.
-			if (key != null && anglebuffer <= 0) {
-				anglebuffer = Math.PI/2;
-				if      (key.getKeyType() == KeyType.Escape) break;
-				else if (key.getKeyType() == KeyType.ArrowRight) {
-					rot = Vector.UNIT_Y;
-					face = Vector.ZERO;
-				} else if (key.getKeyType() == KeyType.ArrowLeft) {
-					rot = Vector.UNIT_Y.scale(-1);
-					face = Vector.ZERO;
-				} else if (key.getKeyType() == KeyType.ArrowDown) {
-					rot = Vector.UNIT_Z;
-					face = Vector.ZERO;
-				} else if (key.getKeyType() == KeyType.ArrowUp) {
-					rot = Vector.UNIT_Z.scale(-1);
-					face = Vector.ZERO;
-				} else if (key.getKeyType() == KeyType.Tab) {
-					if (view == 0) {
-						view = 1;
-						camera.setPos(new Vector(-7, 5, 7));
-						camera.rotate(Vector.UNIT_Y, Math.PI/4);
-					} else if (view == 1) {
-						view = 0;
-						camera.setPos(new Vector(-10, 6, 0));
-						camera.rotate(Vector.UNIT_Y, -Math.PI/4);
-					}
-					anglebuffer = 0; // overriding rotation
-				} else if (key.getCharacter() == 'e') {
-					rot = Vector.UNIT_Y;
-					face = Vector.UNIT_Y;
-				} else if (key.getCharacter() == 'q') {
-					rot = Vector.UNIT_Y.scale(-1);
-					face = Vector.UNIT_Y;
-				} else if (key.getCharacter() == 'c') {
-					rot = Vector.UNIT_Y;
-					face = Vector.UNIT_Y.scale(-1);
-				} else if (key.getCharacter() == 'z') {
-					rot = Vector.UNIT_Y.scale(-1);
-					face = Vector.UNIT_Y.scale(-1);
-				} else if (key.getCharacter() == 'd') {
-					rot = Vector.UNIT_X;
-					face = Vector.UNIT_X.scale(-1);
-				} else if (key.getCharacter() == 'a') {
-					rot = Vector.UNIT_X.scale(-1);
-					face = Vector.UNIT_X.scale(-1);
-				} else if (key.getCharacter() == ' ') {
-					scramble = true;
-					key = null;
-					anglebuffer = 0;
-				} else anglebuffer = 0;
-			} else if (scramble && anglebuffer <= 0) {
-				if (screen.pollInput() != null) scramble = false;
-				anglebuffer = Math.PI/2;
-				int rand = (int)(Math.random()*6);
-					 if (rand == 0) face = Vector.UNIT_X;
-				else if (rand == 1) face = Vector.UNIT_X.scale(-1);
-				else if (rand == 2) face = Vector.UNIT_Y;
-				else if (rand == 3) face = Vector.UNIT_Y.scale(-1);
-				else if (rand == 4) face = Vector.UNIT_Z;
-				else if (rand == 5) face = Vector.UNIT_Z.scale(-1);
-				rot = face.scale((int)(Math.random()*2)*2-1);
-			}
-
-			if (anglebuffer > 0) {
-				for (Shape obj : cubes) {
-					if (face == Vector.ZERO || obj.dir().dotProduct(face) > 0.2)
-						obj.rotate(rot, 0.005*dt);
-				}
-				anglebuffer -= 0.005*dt;
-				if (anglebuffer < 0) { // if we overshot the rotation by a little
-					for (Shape obj : cubes) {
-						if (face == Vector.ZERO || obj.dir().dotProduct(face) > 0.2)
-							obj.rotate(rot, anglebuffer);
-					}
-				}
-			}
-
-			camera.doResizeIfNecessary();
-			camera.clearBuffer();
-			for (Shape obj : cubes) {
-				camera.render(obj);
-			}
-			camera.display();
-		}
-		screen.stopScreen();
 	}
+
+	public String toString() {
+		return "Rubiks(pos: "+pos()+", dir: "+dir()+")";
+	}
+
+	public void render(Camera c) {
+		for (Shape cube : cubes) {
+			c.render(cube);
+		}
+	}
+
+	public void setPos(Vector pos) {
+		super.setPos(pos);
+		Vector rel = this.pos().subtract(pos);
+		for (Shape cube : cubes) {
+			cube.translate(rel);
+		}
+	}
+
+	public void rotate(Vector axis, double angle) {
+		super.rotate(axis, angle);
+		for (Shape cube : cubes) {
+			cube.rotate(axis, angle);
+		}
+	}
+
 }
